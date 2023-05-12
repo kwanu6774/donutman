2f75bade131ab52fbf90f6175262318f31200b33
kwanu6774 <kwanu6774@naver.com>
2023-05-12 17:59:05 +0900

    백준 2480

---
+++ 2480.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#pragma warning (disable:4996)
+
+main() {
+	int n1, n2, n3 ; //주사위 숫자
+	int a; //상금
+
+	if (n1 == n2 == n3) {
+		a = 10000 + n1 * 1000;
+	}
+	else if ( n1 != n2 != n3 ){
+		if (n1 >= n2 | n3)
+
+			a = max_n * 100;
+
+	}
+	else {
+		a= 1000 + max_n * 100
+	}
+	printf("%d", a);
+}
